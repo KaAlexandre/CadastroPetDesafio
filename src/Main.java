8ebb5a72474f83bbe35eab33c12337d29fe0a447
Kauan Alexandre <atomicroyale334@gmail.com>
2025-04-09 18:48:34 -0300

    Funcionalidade 2 - menu inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+        int escolha = -1;
+
+        while (true) {
+            System.out.println("1. Cadastrar um novo pet");
+            System.out.println("2. Alterar os dados do pet cadastrado");
+            System.out.println("3. Deletar um pet cadastrado");
+            System.out.println("4. Listar todos os pets cadastrados");
+            System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+            System.out.println("6. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            if (ler.hasNextInt()) {
+                escolha = ler.nextInt();
+                ler.nextLine();
+                if (escolha >= 1 && escolha <= 6) {
+                    break;
+                }
+            } else {
+                ler.nextLine();
+            }
+
+            System.out.println("Opção inválida! Tente novamente.\n");
+        }
+        switch (escolha) {
+
+            case 1:
+                PrintForm printForm = new PrintForm();
+                Map<String, String> respostas = printForm.printForm();
+                System.out.println("\nRespostas cadastradas:");
+                respostas.forEach((pergunta, resposta) ->
+                        System.out.println(pergunta + ": " + resposta));
+                break;
+            case 6:
+                System.out.println("Saindo...");
+                return;
+        }
+
+
+//
+        ler.close();
     }
 }
